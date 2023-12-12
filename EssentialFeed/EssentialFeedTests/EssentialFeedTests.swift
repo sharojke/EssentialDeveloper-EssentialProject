6e14460f2262dcb3ca096437fda1760189e2afd4
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2023-12-13 00:15:39 +0300

    NM: Request data from URL on 'load'
    Signed-off-by: Vlad Sharaev <sharaev.vl.vl@gmail.com>

--- EssentialFeed/EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeed/EssentialFeedTests/EssentialFeedTests.swift
@@ -1,17 +1,34 @@
 import XCTest
 @testable import EssentialFeed
 
-class RemoteFeedLoader {}
+class RemoteFeedLoader {
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "http://url.com")
+    }
+}
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
