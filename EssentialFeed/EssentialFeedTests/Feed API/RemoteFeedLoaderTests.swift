be4269aed7549bcdec9ddbe3a5568e35c1329929
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2023-12-17 00:10:59 +0300

    NM: Add memory leak tracking to 'XCTestCase' extension

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -191,21 +191,6 @@ extension RemoteFeedLoaderTests {
     ) -> RemoteFeedLoader.Result {
         return .failure(error)
     }
-
-    private func trackForMemoryLeaks(
-        _ instance: AnyObject,
-        file: StaticString = #filePath,
-        line: UInt = #line
-    ) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(
-                instance,
-                "Instance should've been deallocated. Potential memory leak.",
-                file: file,
-                line: line
-            )
-        }
-    }
     
     private func makeItem(
         id: UUID,
--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -161,21 +161,6 @@ extension URLSessionHTTPClientTests {
         
         return sut
     }
-    
-    private func trackForMemoryLeaks(
-        _ instance: AnyObject,
-        file: StaticString = #filePath,
-        line: UInt = #line
-    ) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(
-                instance,
-                "Instance should've been deallocated. Potential memory leak.",
-                file: file,
-                line: line
-            )
-        }
-    }
 }
 
 // swiftlint:enable force_unwrapping
