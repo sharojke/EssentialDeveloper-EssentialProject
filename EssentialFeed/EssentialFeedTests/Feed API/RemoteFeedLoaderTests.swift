b5009ce46bbe07958f274e9d5d3ecb3549223648
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2023-12-16 13:10:53 +0300

    NM: Remove 'LoadFeedResult' conformance from production since it's a test-only requirement

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -188,17 +188,45 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     private func execute(
         _ sut: RemoteFeedLoader,
-        toCompleteWithResult result: RemoteFeedLoader.Result,
+        toCompleteWithResult expectedResult: RemoteFeedLoader.Result,
         when action: () -> Void,
         file: StaticString = #filePath,
         line: UInt = #line
     ) {
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut.load { capturedResults.append($0) }
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedItems), .success(expectedItems)):
+                XCTAssertEqual(
+                    receivedItems,
+                    expectedItems,
+                    file: file,
+                    line: line
+                )
+                
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(
+                    receivedError,
+                    expectedError,
+                    file: file,
+                    line: line
+                )
+                
+            default:
+                XCTFail(
+                    "Expected result \(expectedResult) and got \(receivedResult) instead.",
+                    file: file,
+                    line: line
+                )
+            }
+            
+            exp.fulfill()
+        }
         
         action()
         
-        XCTAssertEqual(capturedResults, [result], file: file, line: line)
+        wait(for: [exp], timeout: 1.0)
     }
     
     private class HTTPClientSpy: HTTPClient {
