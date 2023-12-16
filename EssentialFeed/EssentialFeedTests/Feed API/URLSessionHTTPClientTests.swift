d2d373d52bafef54c95edf6d5274b9d7b2d56e1d
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2023-12-16 21:58:47 +0300

    NM: Resumes data task on creation

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -10,7 +10,7 @@ private final class URLSessionHTTPClient {
     }
     
     public func get(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in }
+        session.dataTask(with: url) { _, _, _ in }.resume()
     }
 }
 
@@ -24,23 +24,49 @@ final class URLSessionHTTPClientTests: XCTestCase {
         
         XCTAssertEqual(session.receivedURLs, [url])
     }
+    
+    func test_getWithUrl_receivesDataTaskWithUrl() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(url: url, task: task)
+        
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url)
+        
+        XCTAssertEqual(task.resumeCallCount, 1)
+    }
 }
 
 extension URLSessionHTTPClientTests {
     private class URLSessionSpy: URLSession {
         var receivedURLs = [URL]()
+        private var stubs = [URL: URLSessionDataTask]()
         
         override func dataTask(
             with url: URL,
             completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
         ) -> URLSessionDataTask {
             receivedURLs.append(url)
-            return FakeURLSessionDataTask()
+            return stubs[url] ?? FakeURLSessionDataTask()
+        }
+        
+        func stub(url: URL, task: URLSessionDataTask) {
+            stubs[url] = task
         }
     }
     
-    private class FakeURLSessionDataTask: URLSessionDataTask {}
+    private class URLSessionDataTaskSpy: URLSessionDataTask {
+        var resumeCallCount = 0
+        
+        override func resume() {
+            resumeCallCount += 1
+        }
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask {
+        override func resume() {}
+    }
 }
 
 // swiftlint:enable force_unwrapping
-// swiftlint:enable force_try
