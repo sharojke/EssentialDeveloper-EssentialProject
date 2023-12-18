b1d93a2d4156d01dedda16bb56acd3bd5d91d94b
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2023-12-18 21:23:11 +0300

    PM: Guarantee no memory leaks in the 'makeSUT' helper method

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -44,9 +44,16 @@ final class CacheFeedUseCaseTests: XCTestCase {
 // MARK: - Helpers
 
 private extension CacheFeedUseCaseTests {
-    func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+    func makeSUT(
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (sut: LocalFeedLoader, store: FeedStore) {
         let store = FeedStore()
         let sut = LocalFeedLoader(store: store)
+        
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
         return (sut, store)
     }
     
