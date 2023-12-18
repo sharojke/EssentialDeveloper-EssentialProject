5b06f3c1482913132203d101a6ce1514959979be
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2023-12-18 21:17:21 +0300

    PM: Save command requests cache deletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -1,11 +1,26 @@
+import EssentialFeed
 import XCTest
 
+// swiftlint:disable force_unwrapping
+
 class LocalFeedLoader {
-    init(store: FeedStore) {}
+    private let store: FeedStore
+    
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
+    }
 }
 
 class FeedStore {
-    let deleteCachedFeedCallCount = 0
+    var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
@@ -15,4 +30,32 @@ final class CacheFeedUseCaseTests: XCTestCase {
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let uniqueItem1 = uniqueItem()
+        let uniqueItem2 = uniqueItem()
+        
+        sut.save([uniqueItem1, uniqueItem2])
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
 }
+
+// MARK: - Helpers
+
+private extension CacheFeedUseCaseTests {
+    func uniqueItem() -> FeedItem {
+        return FeedItem(
+            id: UUID(),
+            imageURL: anyURL()
+        )
+    }
+    
+    func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
+}
+
+// swiftlint:enable force_unwrapping
