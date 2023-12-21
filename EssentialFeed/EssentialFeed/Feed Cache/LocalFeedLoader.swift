e8f5109040dcc39042654cfaf1432ad3bb2722c5
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2023-12-21 21:35:37 +0300

    PM: Extract cache non-deletion side-effect on empty cache from the 'load' method to the 'validateCache' method

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -58,8 +58,15 @@ public final class LocalFeedLoader {
     }
     
     public func validateCache() {
-        store.retrieve { _ in }
-        store.deleteCachedFeed { _ in }
+        store .retrieve { [weak self] result in
+            switch result {
+            case .failure:
+                self?.store.deleteCachedFeed { _ in }
+                
+            default:
+                break
+            }
+        }
     }
     
     private func cache(
