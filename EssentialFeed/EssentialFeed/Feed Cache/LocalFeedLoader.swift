9d182ff968dd847f8a0ad03e8ca8419a961ea0b8
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2023-12-22 21:09:56 +0300

    PM: Move 'FeedCachePrivacy' into a separate file

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -1,22 +1,5 @@
 import Foundation
 
-private enum FeedCachePrivacy {
-    private static let calendar = Calendar(identifier: .gregorian)
-    private static let maxCacheAgeInDays = 7
-    
-    static func validate(_ timestamp: Date, against date: Date) -> Bool {
-        guard let maxCacheAge = calendar.date(
-            byAdding: .day,
-            value: maxCacheAgeInDays,
-            to: timestamp
-        ) else {
-            return false
-        }
-        
-        return date < maxCacheAge
-    }
-}
-
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
