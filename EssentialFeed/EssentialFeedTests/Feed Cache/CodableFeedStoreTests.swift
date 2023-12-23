7084303491655664aa07125bffc2a00548d6a272
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2023-12-23 16:27:45 +0300

    PM: Extract 'storeURL' duplication into a factory method

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -74,13 +74,13 @@ final class CodableFeedStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        let storeURL = FileManager.default.urls(
-            for: .documentDirectory,
-            in: .userDomainMask
-        )
-            .first!
-            .appendingPathComponent("image-feed.store")
-        try? FileManager.default.removeItem(at: storeURL)
+        try? FileManager.default.removeItem(at: storeURL())
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        try? FileManager.default.removeItem(at: storeURL())
     }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -158,15 +158,19 @@ private extension CodableFeedStoreTests {
         file: StaticString = #filePath,
         line: UInt = #line
     ) -> CodableFeedStore {
-        let storeURL = FileManager.default.urls(
-            for: .documentDirectory,
-            in: .userDomainMask
-        )
-            .first!
-            .appendingPathComponent("image-feed.store")
+        let storeURL = storeURL()
         let sut = CodableFeedStore(storeURL: storeURL)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    func storeURL() -> URL {
+        return FileManager.default.urls(
+            for: .documentDirectory,
+            in: .userDomainMask
+        )
+        .first!
+        .appendingPathComponent("image-feed.store")
     }
 }
 
