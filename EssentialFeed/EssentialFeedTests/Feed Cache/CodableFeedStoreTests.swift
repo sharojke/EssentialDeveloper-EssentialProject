2eb70185702c55f3fd950c0c72d26ee70377ea93
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2023-12-23 14:45:22 +0300

    PM: Retrieve command delivers empty on empty cache

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -0,0 +1,29 @@
+import EssentialFeed
+import XCTest
+
+class CodableFeedStore {
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+        completion(.empty)
+    }
+}
+
+final class CodableFeedStoreTests: XCTestCase {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "Wait for cache retrieval")
+        
+        sut.retrieve { result in
+            switch result {
+            case .empty:
+                break
+                
+            default:
+                XCTFail("Expected .empty, got \(result) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+}
