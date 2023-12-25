9b413b7a873907fbe1d0ede1e900bc2fd73d4687
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2023-12-25 23:07:54 +0300

    PM: Fix the 'test_load_deliversItemsSavedOnASeparateInstance' test

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -28,7 +28,7 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
         let sutToPerformLoad = makeSUT()
         let feed = uniqueImageFeed().models
         
-        save(feed, with: sutToPerformLoad)
+        save(feed, with: sutToPerformSave)
     
         expect(sutToPerformLoad, toLoad: feed)
     }
