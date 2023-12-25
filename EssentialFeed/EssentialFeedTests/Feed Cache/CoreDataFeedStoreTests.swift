5c9d2c1f6ced301e4cb91beaebfd9f37c10bb71c
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2023-12-25 16:33:52 +0300

    PM: 'CoreDataFeedStore.retrieve()' delivers no error on non empty cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -35,6 +35,9 @@ final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_insert_deliversNoErrorOnNonEmptyCache() {
+        let sut = makeSUT()
+        
+        assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
     }
     
     func test_insert_overridesPreviouslyInsertedCacheValues() {
