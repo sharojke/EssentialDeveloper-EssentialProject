886ca0b60bf5544c45fdacc6ca379e8f4f743f70
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2023-12-25 15:10:12 +0300

    PM: Add private background context to perform store operations

--- EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -4,12 +4,14 @@ import CoreData
 
 public class CoreDataFeedStore: FeedStore {
     private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
     
     public init(bundle: Bundle = .main) throws {
         container = try NSPersistentContainer.load(
             modelName: "FeedStore",
             in: bundle
         )
+        context = container.newBackgroundContext()
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
