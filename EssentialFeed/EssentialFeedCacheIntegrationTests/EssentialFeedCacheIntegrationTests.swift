7cd564500a0ee042c85aac509b4d60686fefad6b
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2023-12-25 21:52:04 +0300

    PM: 'LocaleFeedLoader' in integration with 'CoreDataFeedStore' delivers no items on empty cache

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -1,5 +1,65 @@
 import EssentialFeed
 import XCTest
 
+// swiftlint:disable force_unwrapping
+// swiftlint:disable force_try
+
 final class EssentialFeedCacheIntegrationTests: XCTestCase {
+    func test_load_deliversNoItemsOnEmptyCache() {
+        let sut = makeSUT()
+        
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case .success(let imageFeed):
+                XCTAssertEqual(
+                    imageFeed,
+                    [],
+                    "Expected empty feed, got \(imageFeed)"
+                )
+                
+            case .failure(let error):
+                XCTFail("Expected successful feed result, got \(error) instead")
+            }
+            
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
 }
+
+// MARK: - Helpers
+
+private extension EssentialFeedCacheIntegrationTests {
+    func makeSUT(
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> LocalFeedLoader {
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let storeURL = testSpecificStoreURL()
+        let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        let sut = LocalFeedLoader(store: store, currentDate: Date.init)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    func testSpecificStoreURL() -> URL {
+        return FileManager.default.urls(
+            for: .cachesDirectory,
+            in: .userDomainMask
+        )
+        .first!
+        .appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    func cachesDirectory() -> URL {
+        return FileManager.default.urls(
+            for: .cachesDirectory,
+            in: .userDomainMask
+        ).first!
+    }
+}
+
+// swiftlint:enable force_unwrapping
+// swiftlint:enable force_try
