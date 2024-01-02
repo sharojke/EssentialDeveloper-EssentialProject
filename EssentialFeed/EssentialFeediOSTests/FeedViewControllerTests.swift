82f83c6663018ffade21db49cb07095b1eed0a0b
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-01-02 20:15:05 +0300

    UIM: Make sure that the refresh control has valid states on various conditions

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -2,7 +2,8 @@ import EssentialFeed
 import UIKit
 import XCTest
 
-final class FeedViewController: UIViewController {
+final class FeedViewController: UITableViewController {
+    private var onViewIsAppearing: ((FeedViewController) -> Void)?
     private var loader: FeedLoader?
     
     convenience init(loader: FeedLoader) {
@@ -14,7 +15,49 @@ final class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(
+            self,
+            action: #selector(refresh),
+            for: .valueChanged
+        )
+        
+        onViewIsAppearing = { viewController in
+            viewController.refresh()
+            viewController.onViewIsAppearing = nil
+        }
+        
+        load()
+    }
+    
+    override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+        
+        onViewIsAppearing?(self)
+    }
+    
+    private func load() {
         loader?.load { _ in }
+    }
+    
+    @objc 
+    private func refresh() {
+        refreshControl?.beginRefreshing()
+        load()
+    }
+}
+
+private final class FakeRefreshControl: UIRefreshControl {
+    private var _isRefreshing = false
+    
+    override var isRefreshing: Bool { _isRefreshing }
+    
+    override func beginRefreshing() {
+        _isRefreshing = true
+    }
+    
+    override func endRefreshing() {
+        _isRefreshing = false
     }
 }
 
@@ -31,6 +74,21 @@ final class FeedViewControllerTests: XCTestCase {
         sut.loadViewIfNeeded()
         
         XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    
+    func test_refreshControl_hasValidStatesOnVariousConditions() {
+        let (sut, _) = makeSUT()
+        
+        sut.simulateAppearance()
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
+        
+        sut.refreshControl?.endRefreshing()
+        sut.refreshControl?.simulatePullToRefresh()
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
+        
+        sut.refreshControl?.endRefreshing()
+        sut.simulateAppearance()
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
     }
 }
 
@@ -57,5 +115,51 @@ private extension FeedViewControllerTests {
         trackForMemoryLeaks(loader, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, loader)
+    }
+}
+
+private extension FeedViewController {
+    func simulateAppearance() {
+        if !isViewLoaded {
+            loadViewIfNeeded()
+            replaceRefreshControlWithFakeForiOS17Support()
+        }
+        
+        beginAppearanceTransition(true, animated: false)
+        endAppearanceTransition()
+    }
+    
+    func replaceRefreshControlWithFakeForiOS17Support() {
+        let fake = FakeRefreshControl()
+        
+        refreshControl?.allTargets.forEach { target in
+            refreshControl?.actions(
+                forTarget: target,
+                forControlEvent: .valueChanged
+            )?
+                .forEach { action in
+                    fake.addTarget(
+                        target,
+                        action: Selector(action),
+                        for: .valueChanged
+                    )
+                }
+        }
+        
+        refreshControl = fake
+    }
+}
+
+private extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        allTargets.forEach { target in
+            actions(
+                forTarget: target,
+                forControlEvent: .valueChanged
+            )?
+                .forEach { action in
+                    (target as NSObject).perform(Selector(action))
+                }
+        }
     }
 }
