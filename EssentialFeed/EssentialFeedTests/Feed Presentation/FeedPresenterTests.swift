57d5be1dfe15e95884212d4bc1b32af81e688506
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-01-20 14:41:46 +0300

    UIM: Extract `system under test` creation to a test factory method

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -7,9 +7,7 @@ final class FeedPresenter {
 
 final class FeedPresenterTests: XCTestCase {
     func test_init_doesNotSendMessagesToView() {
-        let view = ViewSpy()
-        
-        _ = FeedPresenter(view: view)
+        let (_, view) = makeSUT()
         
         XCTAssertTrue(
             view.messages.isEmpty,
@@ -23,5 +21,16 @@ final class FeedPresenterTests: XCTestCase {
 private extension FeedPresenterTests {
     final class ViewSpy {
         let messages = [Any]()
+    }
+    
+    func makeSUT(
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (sut: FeedPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedPresenter(view: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
     }
 }
