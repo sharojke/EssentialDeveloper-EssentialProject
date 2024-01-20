0729fb047667be1507567036630bd3034e0b8187
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-01-20 16:09:17 +0300

    UIM: `FeedPresenter` displays no error message on did start loading feed

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -1,7 +1,38 @@
 import XCTest
 
+protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
+struct FeedErrorViewModel {
+    static var noError: Self {
+        return Self(message: nil)
+    }
+
+    let message: String?
+}
+
 final class FeedPresenter {
-    init(view: Any) {
+    private let errorView: FeedErrorView
+    
+    init(errorView: FeedErrorView) {
+        self.errorView = errorView
+    }
+    
+    func didStartLoadingFeed() {
+        errorView.display(.noError)
+    }
+}
+
+final class ViewSpy: FeedErrorView {
+    enum Message: Equatable {
+        case display(errorMessage: String?)
+    }
+    
+    private(set) var messages = [Message]()
+    
+    func display(_ viewModel: FeedErrorViewModel) {
+        messages.append(.display(errorMessage: viewModel.message))
     }
 }
 
@@ -14,21 +45,28 @@ final class FeedPresenterTests: XCTestCase {
             "Expected no view messages on initialization"
         )
     }
+    
+    func test_didStartLoadingFeed_displaysNoErrorMessage() {
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoadingFeed()
+        
+        XCTAssertEqual(
+            view.messages,
+            [.display(errorMessage: nil)]
+        )
+    }
 }
 
 // MARK: - Helpers
 
 private extension FeedPresenterTests {
-    final class ViewSpy {
-        let messages = [Any]()
-    }
-    
     func makeSUT(
         file: StaticString = #filePath,
         line: UInt = #line
     ) -> (sut: FeedPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedPresenter(view: view)
+        let sut = FeedPresenter(errorView: view)
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
