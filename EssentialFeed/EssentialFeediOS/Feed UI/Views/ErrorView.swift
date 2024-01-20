99917f0d143617c80eee7e06b373f99206d7af17
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-01-20 15:32:00 +0300

    UIM: Show and Hide error message animated

--- EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -2,15 +2,46 @@ import UIKit
 
 public final class ErrorView: UIView {
     public var message: String? {
-        get { return label.text }
-        set { label.text = newValue }
+        get { return isVisible ? label.text : nil }
+        set { setMessageAnimated(newValue) }
     }
-
+    
+    private var isVisible: Bool {
+        return alpha > 0
+    }
+    
     @IBOutlet private var label: UILabel!
     
     override public func awakeFromNib() {
         super.awakeFromNib()
-
+        
         label.text = nil
+        alpha = 0
+    }
+    
+    private func setMessageAnimated(_ message: String?) {
+        if let message {
+            showAnimated(message)
+        } else {
+            hideMessageAnimated()
+        }
+    }
+    
+    private func showAnimated(_ message: String) {
+        label.text = message
+        
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+    
+    private func hideMessageAnimated() {
+        UIView.animate(
+            withDuration: 0.25,
+            animations: { self.alpha = 0 },
+            completion: { completed in
+                if completed { self.label.text = nil }
+            }
+        )
     }
 }
