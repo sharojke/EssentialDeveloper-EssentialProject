d7b8b4be1e0f18c60e089a9385cf3acf477e3cc1
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-01-20 15:00:47 +0300

    UIM: Add loco for the error message

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/Models/FeedErrorViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Macbook Gray on 1/20/24.
+//
+
+import Foundation
---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Macbook Gray on 1/20/24.
+//
+
+import Foundation
