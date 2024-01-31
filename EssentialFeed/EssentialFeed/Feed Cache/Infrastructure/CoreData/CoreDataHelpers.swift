e0f8f1a677ba318cdf54479b1b7dbcba1baad22c
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-01-31 23:44:32 +0300

    MM: Load `NSManagedObjectModel` instance lazily and cache it to prevent multiple `NSEntityDescriptions` claiming the same `NSManagedObject` model subclasses (This problem just generates warnings but could lead to undefined behavior in the future)

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataHelpers.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataHelpers.swift
@@ -1,33 +1,24 @@
 import CoreData
 
 extension NSPersistentContainer {
-    enum LoadingError: Error {
-        case modelNotFound
-        case failedToLoadPersistentStores(Error)
-    }
-    
     static func load(
-        modelName name: String,
-        url: URL,
-        in bundle: Bundle
+        name: String,
+        model: NSManagedObjectModel,
+        url: URL
     ) throws -> Self {
-        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
-            throw(LoadingError.modelNotFound)
-        }
-        
         let description = NSPersistentStoreDescription(url: url)
         let container = Self(name: name, managedObjectModel: model)
         container.persistentStoreDescriptions = [description]
         
         var loadError: Error?
         container.loadPersistentStores { loadError = $1 }
-        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+        try loadError.map { throw $0 }
         
         return container
     }
 }
 
-private extension NSManagedObjectModel {
+extension NSManagedObjectModel {
     static func with(name: String, in bundle: Bundle) -> Self? {
         return bundle
             .url(forResource: name, withExtension: "momd")
