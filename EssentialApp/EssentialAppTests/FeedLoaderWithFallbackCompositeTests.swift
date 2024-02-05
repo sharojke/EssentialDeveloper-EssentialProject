a66d1ef4ee5d0d634a7419806813c7574d566363
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-02-05 23:13:00 +0300

    MM: Extract system under test (SUT) creation into a factory method

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -33,11 +33,9 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
         let primaryFeed = uniqueFeed()
         let fallbackFeed = uniqueFeed()
-        let primaryLoader = LoaderStub(result: .success(primaryFeed))
-        let fallbackLoader = LoaderStub(result: .success(fallbackFeed))
-        let sut = FeedLoaderWithFallbackComposite(
-            primaryLoader: primaryLoader,
-            fallbackLoader: fallbackLoader
+        let sut = makeSUT(
+            primaryResult: .success(primaryFeed),
+            fallbackResult: .success(fallbackFeed)
         )
         
         let exp = expectation(description: "Wait for load completion")
@@ -58,6 +56,39 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 // MARK: - Helpers
 
 private extension FeedLoaderWithFallbackCompositeTests {
+    func makeSUT(
+        primaryResult: FeedLoader.Result,
+        fallbackResult: FeedLoader.Result,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> FeedLoader {
+        let primaryLoader = LoaderStub(result: primaryResult)
+        let fallbackLoader = LoaderStub(result: fallbackResult)
+        let sut = FeedLoaderWithFallbackComposite(
+            primaryLoader: primaryLoader,
+            fallbackLoader: fallbackLoader
+        )
+        trackForMemoryLeaks(primaryLoader, file: file, line: line)
+        trackForMemoryLeaks(fallbackLoader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    func trackForMemoryLeaks(
+        _ instance: AnyObject,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(
+                instance,
+                "Instance should have been deallocated. Potential memory leak",
+                file: file,
+                line: line
+            )
+        }
+    }
+    
     func uniqueFeed() -> [FeedImage] {
         return [
             FeedImage(
