5e1314f692a4fa149ec69903108f3d1d75a2629e
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-02-08 22:08:41 +0300

    MM: `FeedLoaderCacheDecorator.load` delivers decoratee loader result (either success or failure)

---
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -0,0 +1,103 @@
+import EssentialFeed
+import XCTest
+
+private class LoaderStub: FeedLoader {
+    private let result: FeedLoader.Result
+    
+    init(result: FeedLoader.Result) {
+        self.result = result
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        completion(result)
+    }
+}
+
+final class FeedLoaderCacheDecorator: FeedLoader {
+    private let decoratee: FeedLoader
+    
+    init(decoratee: FeedLoader) {
+        self.decoratee = decoratee
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load(completion: completion)
+    }
+}
+
+final class FeedLoaderCacheDecoratorTests: XCTestCase {
+    func test_load_deliversFeedOnLoaderSuccess() {
+        let feed = uniqueFeed()
+        let sut = makeSUT(loaderResult: .success(feed))
+        
+        expect(sut, toCompleteWithResult: .success(feed))
+    }
+    
+    func test_load_deliversErrorOnLoaderFailure() {
+        let feed = uniqueFeed()
+        let sut = makeSUT(loaderResult: .failure(anyNSError()))
+        
+        expect(sut, toCompleteWithResult: .failure(anyNSError()))
+    }
+}
+
+// MARK: - Helpers
+
+private extension FeedLoaderCacheDecoratorTests {
+    func makeSUT(
+        loaderResult: FeedLoader.Result,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> FeedLoader {
+        let loader = LoaderStub(result: loaderResult)
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    func expect(
+        _ sut: FeedLoader,
+        toCompleteWithResult expectedResult: FeedLoader.Result,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedFeed), .success(expectedFeed)):
+                XCTAssertEqual(
+                    receivedFeed,
+                    expectedFeed,
+                    "Expected \(expectedResult), got \(receivedResult) instead",
+                    file: file,
+                    line: line
+                )
+                
+            case (.failure, .failure):
+                break
+                
+            default:
+                XCTFail(
+                    "Expected \(expectedResult), got \(receivedResult) instead",
+                    file: file,
+                    line: line
+                )
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
+    }
+    
+    func uniqueFeed() -> [FeedImage] {
+        return [
+            FeedImage(
+                id: UUID(),
+                url: anyURL(),
+                description: "any",
+                location: "any"
+            )
+        ]
+    }
+}
