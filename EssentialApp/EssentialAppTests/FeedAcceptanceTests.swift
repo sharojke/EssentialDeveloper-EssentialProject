92da98baa0d1341a000500cc74a4d256df903a46
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-02-13 23:24:17 +0300

    MM: Move `InMemoryFeedStore` to a shared scope in a separate file

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -7,61 +7,6 @@ import XCTest
 // swiftlint:disable force_unwrapping
 // swiftlint:disable force_try
 
-private class InMemoryFeedStore: FeedStore, FeedImageDataStore {
-    static var empty: InMemoryFeedStore {
-        InMemoryFeedStore()
-    }
-    
-    static var withExpiredFeedCache: InMemoryFeedStore {
-        InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date.distantPast))
-    }
-    
-    static var withNonExpiredFeedCache: InMemoryFeedStore {
-        InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date()))
-    }
-    
-    private(set) var feedCache: CachedFeed?
-    private var feedImageDataCache: [URL: Data] = [:]
-    
-    private init(feedCache: CachedFeed? = nil) {
-        self.feedCache = feedCache
-    }
-    
-    func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletion) {
-        feedCache = nil
-        completion(.success(()))
-    }
-    
-    func insert(
-        feed: [LocalFeedImage],
-        timestamp: Date,
-        completion: @escaping FeedStore.InsertionCompletion
-    ) {
-        feedCache = CachedFeed(feed: feed, timestamp: timestamp)
-        completion(.success(()))
-    }
-    
-    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
-        completion(.success(feedCache))
-    }
-    
-    func insert(
-        _ data: Data,
-        for url: URL,
-        completion: @escaping (FeedImageDataStore.InsertionResult) -> Void
-    ) {
-        feedImageDataCache[url] = data
-        completion(.success(()))
-    }
-    
-    func retrieve(
-        dataForURL url: URL,
-        completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void
-    ) {
-        completion(.success(feedImageDataCache[url]))
-    }
-}
-
 final class FeedAcceptanceTests: XCTestCase {
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let feed = launch(
