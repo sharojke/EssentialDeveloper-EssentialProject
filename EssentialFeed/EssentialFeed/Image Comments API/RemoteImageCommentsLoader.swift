9287326fa7ba9ac7266992ede1a0800376f5350c
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-02-27 22:11:05 +0300

    LS-1: Replace `RemoteImageCommentsLoader` with generic `RemoteLoader` to remove duplication

--- EssentialFeed/EssentialFeed/Image Comments API/RemoteImageCommentsLoader.swift
+++ EssentialFeed/EssentialFeed/Image Comments API/RemoteImageCommentsLoader.swift
@@ -1,41 +1,10 @@
-import Foundation
+public typealias RemoteImageCommentsLoader = RemoteLoader<[ImageComment]>
 
-public final class RemoteImageCommentsLoader {
-    public typealias Result = Swift.Result<[ImageComment], Swift.Error>
-    
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    private let url: URL
-    private let client: HTTPClient
-    
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
-        do {
-            let remote = try ImageCommentsMapper.map(data, from: response)
-            return .success(remote)
-        } catch {
-            return .failure(error)
-        }
-    }
-    
-    public func load(completion: @escaping (Result) -> Void) {
-        _ = client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            
-            switch result {
-            case .success((let data, let response)):
-                completion(Self.map(data, from: response))
-                
-            case .failure:
-                completion(.failure(Error.connectivity))
-            }
-        }
+public extension RemoteImageCommentsLoader {
+    convenience init(
+        url: URL,
+        client: HTTPClient
+    ) {
+        self.init(url: url, client: client, mapper: ImageCommentsMapper.map)
     }
 }
