0e97e9651623e6ea03e9cc2fba7a0742830adb68
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-02-27 22:13:34 +0300

    LS-1: Replace `RemoteFeedLoader` with generic `RemoteLoader` to remove duplication

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -1,41 +1,10 @@
-import Foundation
+public typealias RemoteFeedLoader = RemoteLoader<[FeedImage]>
 
-public final class RemoteFeedLoader: FeedLoader {
-    public typealias Result = FeedLoader.Result
-    
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    private let url: URL
-    private let client: HTTPClient
-    
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
-        do {
-            let images = try FeedItemsMapper.map(data, from: response)
-            return .success(images)
-        } catch {
-            return .failure(error)
-        }
-    }
-    
-    public func load(completion: @escaping (Result) -> Void) {
-        _ = client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            
-            switch result {
-            case .success((let data, let response)):
-                completion(Self.map(data, from: response))
-                
-            case .failure:
-                completion(.failure(Error.connectivity))
-            }
-        }
+public extension RemoteFeedLoader {
+    convenience init(
+        url: URL,
+        client: HTTPClient
+    ) {
+        self.init(url: url, client: client, mapper: FeedItemsMapper.map)
     }
 }
--- EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -5,44 +5,6 @@ import XCTest
 // swiftlint:disable force_try
 
 final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
-    func test_init_doesNotRequestDataFromURL() {
-        let (_, client) = makeSUT()
-        
-        XCTAssertTrue(client.requestedURLs.isEmpty)
-    }
-    
-    func test_load_requestsDataFromURL() {
-        let url = URL(string: "http://given-url.com")!
-        let (sut, client) = makeSUT(url: url)
-        
-        sut.load { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url])
-    }
-    
-    func test_loadTwice_requestsDataFromUrlTwice() {
-        let url = URL(string: "http://given-url.com")!
-        let (sut, client) = makeSUT(url: url)
-        
-        sut.load { _ in }
-        sut.load { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url, url])
-    }
-    
-    func test_load_deliversErrorOnClientError() {
-        let (sut, client) = makeSUT()
-        
-        execute(
-            sut,
-            toCompleteWithResult: failure(.connectivity),
-            when: {
-                let clientError = NSError(domain: "Test", code: 0)
-                client.complete(with: clientError)
-            }
-        )
-    }
-    
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
         
@@ -111,23 +73,6 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
                 client.complete(withStatusCode: 200, data: json)
             }
         )
-    }
-    
-    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-        let url = URL(string: "http://url.com")!
-        let client = HTTPClientSpy()
-        var sut: RemoteFeedLoader? = RemoteFeedLoader(
-            url: url,
-            client: client
-        )
-        
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut?.load { capturedResults.append($0) }
-        
-        sut = nil
-        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
-        
-        XCTAssertTrue(capturedResults.isEmpty)
     }
 }
 
