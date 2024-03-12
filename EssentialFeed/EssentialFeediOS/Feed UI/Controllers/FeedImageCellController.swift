ca0890c10266a463db9f08879ec6c2dac1f788e4
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-03-12 23:33:50 +0300

    LS-3: Replace deprecated `traitCollectionDidChange` with `registerForTraitChanges`

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -73,6 +73,11 @@ extension FeedImageCellController: UITableViewDataSource,
         return cell!
     }
     
+    public func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        self.cell = cell as? FeedImageCell
+        delegate.didRequestImage()
+    }
+    
     public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         delegate.didRequestImage()
     }
