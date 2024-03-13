963af1b511fbd09b639ab767960bf03f07c7580b
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-03-13 23:13:17 +0300

    LS-4: Move localization helpers to shared scope and remove feed references from comments integration tests

--- EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
@@ -30,7 +30,7 @@ private class ThisLoaderSpy {
     }
 }
 
-final class CommentsUIIntegrationTests: FeedUIIntegrationTests {
+final class CommentsUIIntegrationTests: XCTestCase {
     func test_commentsView_hasTitle() {
         let (sut, _) = makeSUT()
         
@@ -191,7 +191,7 @@ final class CommentsUIIntegrationTests: FeedUIIntegrationTests {
         wait(for: [exp], timeout: 1.0)
     }
     
-    override func test_loadFeedCompletion_rendersErrorMessageOnErrorUntilNextReload() {
+    func test_loadCommentsCompletion_rendersErrorMessageOnErrorUntilNextReload() {
         let (sut, loader) = makeSUT()
         
         sut.simulateAppearance()
@@ -204,7 +204,7 @@ final class CommentsUIIntegrationTests: FeedUIIntegrationTests {
         XCTAssertEqual(sut.errorMessage, nil)
     }
     
-    override func test_tapOnErrorView_hidesErrorMessage() {
+    func test_tapOnErrorView_hidesErrorMessage() {
         let (sut, loader) = makeSUT()
         
         sut.simulateAppearance()
--- EssentialApp/EssentialAppTests/Helpers/SharediOSTestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/SharediOSTestHelpers.swift
@@ -1,6 +1,23 @@
+import EssentialFeed
 import UIKit
 
 // swiftlint:disable force_unwrapping
+
+private final class DummyView: ResourceView {
+    func display(_ viewModel: Any) {}
+}
+
+var loadError: String {
+    return LoadResourcePresenter<Any, DummyView>.loadError
+}
+
+var feedTitle: String {
+    return FeedPresenter.title
+}
+
+var commentsTitle: String {
+    return ImageCommentsPresenter.title
+}
 
 func anyImageData() -> Data {
     return UIImage.make(withColor: .red).pngData()!
