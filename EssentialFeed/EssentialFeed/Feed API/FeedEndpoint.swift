f0d614bd6ae61a6d763dfe391b513bc683d69f8a
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-03-21 21:40:11 +0300

    LS-5: Add feed endpoint with "after_id" param

--- EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
@@ -1,18 +1,19 @@
 import Foundation
 
 public enum FeedEndpoint {
-    case get
-        
+    case get(after: FeedImage? = nil)
+
     public func url(baseURL: URL) -> URL {
         switch self {
-        case .get:
+        case .get(let image):
             var components = URLComponents()
             components.scheme = baseURL.scheme
             components.host = baseURL.host
             components.path = baseURL.path + "/v1/feed"
             components.queryItems = [
-                URLQueryItem(name: "limit", value: "10")
-            ]
+                URLQueryItem(name: "limit", value: "10"),
+                image.map { URLQueryItem(name: "after_id", value: $0.id.uuidString) }
+            ].compactMap { $0 }
             // swiftlint:disable:next force_unwrapping
             return components.url!
         }
