1b5fdc2781529215cdcb4bfbff0386f8cfd47104
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-03-30 16:54:18 +0300

    LS-7: Deprecate async APIs in favor of new sync APIs

--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -14,10 +14,12 @@ public protocol FeedStore {
     
     /// The completion handler can be invoked in any thread
     /// Clients are responsible to dispatch to appropriate threads, if needed
+    @available(*, deprecated)
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
     
     /// The completion handler can be invoked in any thread
     /// Clients are responsible to dispatch to appropriate threads, if needed
+    @available(*, deprecated)
     func insert(
         feed: [LocalFeedImage],
         timestamp: Date,
@@ -26,5 +28,58 @@ public protocol FeedStore {
     
     /// The completion handler can be invoked in any thread
     /// Clients are responsible to dispatch to appropriate threads, if needed
+    @available(*, deprecated)
     func retrieve(completion: @escaping RetrievalCompletion)
+    
+    func deleteCachedFeed() throws
+    func insert(feed: [LocalFeedImage], timestamp: Date) throws
+    func retrieve() throws -> CachedFeed?
+}
+
+public extension FeedStore {
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {}
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {}
+    func retrieve(completion: @escaping RetrievalCompletion) {}
+    
+    func deleteCachedFeed() throws {
+        let dispatchGroup = DispatchGroup()
+        
+        // swiftlint:disable:next implicitly_unwrapped_optional
+        var result: DeletionResult!
+        dispatchGroup.enter()
+        deleteCachedFeed { receivedResult in
+            result = receivedResult
+            dispatchGroup.leave()
+        }
+        dispatchGroup.wait()
+        return try result.get()
+    }
+    
+    func insert(feed: [LocalFeedImage], timestamp: Date) throws {
+        let dispatchGroup = DispatchGroup()
+        
+        // swiftlint:disable:next implicitly_unwrapped_optional
+        var result: InsertionResult!
+        dispatchGroup.enter()
+        insert(feed: feed, timestamp: timestamp) { receivedResult in
+            result = receivedResult
+            dispatchGroup.leave()
+        }
+        dispatchGroup.wait()
+        return try result.get()
+    }
+    
+    func retrieve() throws -> CachedFeed? {
+        let dispatchGroup = DispatchGroup()
+        
+        // swiftlint:disable:next implicitly_unwrapped_optional
+        var result: RetrievalResult!
+        dispatchGroup.enter()
+        retrieve() { receivedResult in
+            result = receivedResult
+            dispatchGroup.leave()
+        }
+        dispatchGroup.wait()
+        return try result.get()
+    }
 }
