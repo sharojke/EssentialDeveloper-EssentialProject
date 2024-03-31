083b5b6bd3cf9db0bf965d5b1f1f3b2c8e64497c
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-03-31 14:56:32 +0300

    LS-7: Replace NullStore with InMemoryFeedStore to improve UX - The InMemoryStore can provide better UX as it supports pagination and image caching - Made the infra queue serial - instead of concurrent - since the InMemoryFeedStore is not thread safe

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -15,8 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         return DispatchQueue(
             label: "com.essentialdeveloper.infra.queue",
-            qos: .userInitiated,
-            attributes: .concurrent
+            qos: .userInitiated
         ).eraseToAnyScheduler()
     }()
     
@@ -39,7 +38,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         } catch {
             assertionFailure("Failed to instantiate CoreData store with error: \(error.localizedDescription)")
             logger.fault("Failed to instantiate CoreData store with error: \(error.localizedDescription)")
-            return NullStore()
+            return InMemoryFeedStore()
         }
     }()
     
